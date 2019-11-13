23b0b0f8f65e9bca92f980f0bba4e42378aabe84
Michael <michael@robertsmj.com>
2019-11-13 14:09:07 -0700

    “added-these-files”

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,17 @@ const pkg = require('./package')
 
 module.exports = {
   mode: 'universal',
-
+  generate: {
+    routes: function () {
+      const fs = require('fs');
+      return fs.readdirSync('./assets/content/blog').map(file => {
+        return {
+          route: `/blog/${file.slice(2, -5)}`, // Remove the .json from the end of the filename
+          payload: require(`./assets/content/blog/${file}`),
+        };
+      });
+    },
+  },
   /*
   ** Headers of the page
   */
