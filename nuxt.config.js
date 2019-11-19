fbf2057666c21f2871b0f50d9b7d5c9eb8410fa3
Michael <michael@robertsmj.com>
2019-11-19 16:14:02 -0700

    all

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,69 +1,78 @@
 const pkg = require('./package');
 
 module.exports = {
-  mode: 'universal',
-  generate: {
-
-  },
-  /*
-  ** Headers of the page
-  */
-  head: {
-    htmlAttrs: {
-      lang: 'en'
+    mode: 'universal',
+    generate: {
+        generate: {
+            routes: function () {
+                const fs = require('fs');
+                return fs.readdirSync('./assets/content/portfolio').map(file => {
+                    return {
+                    route: `/blog/${file.slice(2, -5)}`, // Remove the .json from the end of the filename
+                        payload: require(`./assets/content/portfolio/${file}`),
+                    };
+                });
+            },
+        },
     },
-    title: pkg.name,
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+    /*
+    ** Headers of the page
+    */
+    head: {
+        htmlAttrs: {
+            lang: 'en'
+        },
+        title: pkg.name,
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            { hid: 'description', name: 'description', content: pkg.description }
+        ],
+        link: [
+            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+        ]
+    },
+    /*
+    ** Customize the progress-bar color
+    */
+    loading: { color: '#fff' },
+
+    /*
+    ** Global CSS
+    */
+    css: [
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
 
-  /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#fff' },
+    /*
+    ** Plugins to load before mounting the App
+    */
+    plugins: [
+        { src: '~plugins/vue-carousel', ssr: false }
+    ],
 
-  /*
-  ** Global CSS
-  */
-  css: [
-  ],
+    /*
+    ** Nuxt.js modules
+    */
+    modules: [
+        // Doc: https://github.com/nuxt-community/axios-module#usage
+        '@nuxtjs/axios',
+        '@nuxtjs/markdownit'
+    ],
+    markdownit: {
+        injected: true,
+    },
+    /*
 
-  /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-    { src: '~plugins/vue-carousel', ssr: false }
-  ],
+    ** Axios module configuration
+    */
+    axios: {
+        // See https://github.com/nuxt-community/axios-module#options
+    },
 
-  /*
-  ** Nuxt.js modules
-  */
-  modules: [
-    // Doc: https://github.com/nuxt-community/axios-module#usage
-    '@nuxtjs/axios',
-    '@nuxtjs/markdownit'
-  ],
-  markdownit: {
-    injected: true,
-  },
-  /*
-  
-  ** Axios module configuration
-  */
-  axios: {
-    // See https://github.com/nuxt-community/axios-module#options
-  },
-
-  /*
-  ** Build configuration
-  */
-  build: {
-    vendor: ['vue-carousel']
-  }
+    /*
+    ** Build configuration
+    */
+    build: {
+        vendor: ['vue-carousel']
+    }
 }
